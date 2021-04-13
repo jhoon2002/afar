5b9b494a88dde722612500546a3e14d827b1e2dd
jhoon2002 <jhoon2002@naver.com>
2021-04-14 08:15:36 +0900

    notebook

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 //import { AclRule } from 'vue-acl'
 import Blank from "@/views/Blank.vue"
 import Mock from "@/views/Mock.vue"
+import AdminLayout from "@/views/admin/Layout.vue"
 
 Vue.use(VueRouter)
 
@@ -45,7 +46,7 @@ const routes = [
   {
     name: "첫화면",
     path: "/",
-    component: Blank,
+    component: () => import("@/views/Main.vue"),
     invisible: true,
     meta: {
       rule: ['*']
@@ -55,7 +56,7 @@ const routes = [
     name: "게시판관리",
     icon: "mdi-view-headline",
     path: "/admin/board",
-    component: Blank,
+    component: AdminLayout,
     isShow: false,
     isOn: false,
     meta: { isDirectory: true },
@@ -69,7 +70,7 @@ const routes = [
         isOn: false,
         meta: {
           model: "board",
-          rule: ["admin"]
+          rule: ["*"]
         }
       },
       {
@@ -79,7 +80,10 @@ const routes = [
         props: true,
         isShow: false,
         isOn: false,
-        meta: { model: "board" }
+        meta: {
+          model: "board",
+          rule: ["*"]
+        }
       },
       {
         name: "자유게시판",
@@ -88,7 +92,10 @@ const routes = [
         props: true,
         isShow: false,
         isOn: false,
-        meta: { model: "board" }
+        meta: {
+          model: "board",
+          rule: ["*"]
+        }
       }
     ]
   },
@@ -96,7 +103,7 @@ const routes = [
     name: "문서관리",
     icon: "mdi-file-document-edit-outline",
     path: "/admin/document",
-    component: Blank,
+    component: AdminLayout,
     isShow: false,
     isOn: false,
     meta: { isDirectory: true },
@@ -108,7 +115,10 @@ const routes = [
         props: true,
         isShow: false,
         isOn: false,
-        meta: { model: "document" }
+        meta: {
+          model: "document",
+          rule: ["*"]
+        }
       },
       {
         name: "tiptap",
@@ -117,7 +127,10 @@ const routes = [
         props: true,
         isShow: false,
         isOn: false,
-        meta: { model: "document" }
+        meta: {
+          model: "document",
+          rule: ["*"]
+        }
       },
       {
         name: "tiptapEx",
@@ -126,7 +139,10 @@ const routes = [
         props: true,
         isShow: false,
         isOn: false,
-        meta: { model: "document" }
+        meta: {
+          model: "document",
+          rule: ["*"]
+        }
       },
       /*
       {
@@ -199,7 +215,7 @@ const routes = [
     name: "교직원관리",
     icon: "mdi-card-account-details-outline",
     path: "/admin/user",
-    component: Blank,
+    component: AdminLayout,
     isShow: false,
     isOn: false,
     meta: { isDirectory: true },
@@ -211,21 +227,30 @@ const routes = [
         props: true,
         isShow: false,
         isOn: false,
-        meta: { model: "employee" }
+        meta: {
+          model: "employee",
+          rule: ["*"]
+        }
       },
       {
         name: "권한관리",
         path: "/admin/user/acl",
         component: Mock,
         isShow: false,
-        isOn: false
+        isOn: false,
+        meta: {
+          rule: ["*"]
+        }
       },
       {
         name: "테스트",
         path: "/admin/user/test",
         component: Mock,
         isShow: false,
-        isOn: false
+        isOn: false,
+        meta: {
+          rule: ["*"]
+        }
       }
     ]
   },
@@ -233,7 +258,7 @@ const routes = [
     name: "인사관리",
     icon: "mdi-account-circle",
     path: "/admin/person",
-    component: Blank,
+    component: AdminLayout,
     isShow: false,
     isOn: false,
     meta: {
@@ -246,6 +271,9 @@ const routes = [
         component: Mock,
         isShow: false,
         isOn: false,
+        meta: {
+          rule: ["*"]
+        }
       },
       {
         name: "증명서",
@@ -256,19 +284,26 @@ const routes = [
         meta: {
           isDirectory: true
         },
-        children: [{
-          name: "증명서현황",
-          path: "/admin/person/certi/list",
-          component: Mock,
-          isShow: false,
-          isOn: false
-        },
+        children: [
+          {
+            name: "증명서현황",
+            path: "/admin/person/certi/list",
+            component: Mock,
+            isShow: false,
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
+          },
           {
             name: "증명서작성",
             path: "/admin/person/certi/wirte",
             component: Mock,
             isShow: false,
-            isOn: false
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
           }
         ]
       },
@@ -281,19 +316,26 @@ const routes = [
         meta: {
           isDirectory: true
         },
-        children: [{
-          name: "급여현황",
-          path: "/admin/person/pay/list",
-          component: Mock,
-          isShow: false,
-          isOn: false
-        },
+        children: [
+          {
+            name: "급여현황",
+            path: "/admin/person/pay/list",
+            component: Mock,
+            isShow: false,
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
+          },
           {
             name: "급여입력",
             path: "/admin/person/pay/write",
             component: Mock,
             isShow: false,
-            isOn: false
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
           }
         ]
       }
@@ -303,37 +345,45 @@ const routes = [
     name: "복무관리",
     icon: "mdi-bus-multiple",
     path: "/admin/work",
-    component: Blank,
+    component: AdminLayout,
     isShow: false,
     isOn: false,
     meta: {
       isDirectory: true
     },
-    children: [{
-      name: "휴가",
-      path: "/admin/work/off",
-      component: Blank,
-      isShow: false,
-      isOn: false,
-      meta: {
-        isDirectory: true
-      },
-      children: [{
-        name: "휴가설정",
-        path: "/admin/work/off/set",
-        component: Mock,
+    children: [
+      {
+        name: "휴가",
+        path: "/admin/work/off",
+        component: Blank,
         isShow: false,
-        isOn: false
+        isOn: false,
+        meta: {
+          isDirectory: true
+        },
+        children: [
+          {
+            name: "휴가설정",
+            path: "/admin/work/off/set",
+            component: Mock,
+            isShow: false,
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
+          },
+          {
+            name: "휴가현황",
+            path: "/admin/work/off/book-list",
+            component: Mock,
+            isShow: false,
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
+          }
+        ]
       },
-        {
-          name: "휴가현황",
-          path: "/admin/work/off/book-list",
-          component: Mock,
-          isShow: false,
-          isOn: false
-        }
-      ]
-    },
       {
         name: "시간외근무",
         path: "/admin/work/overtime",
@@ -343,19 +393,26 @@ const routes = [
         meta: {
           isDirectory: true
         },
-        children: [{
-          name: "시간외근무설정",
-          path: "/admin/work/overtime/set",
-          component: Mock,
-          isShow: false,
-          isOn: false
-        },
+        children: [
+          {
+            name: "시간외근무설정",
+            path: "/admin/work/overtime/set",
+            component: Mock,
+            isShow: false,
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
+          },
           {
             name: "시간외근무현황",
             path: "/admin/work/overtime/pre-list",
             component: Mock,
             isShow: false,
-            isOn: false
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
           }
         ]
       },
@@ -368,13 +425,18 @@ const routes = [
         meta: {
           isDirectory: true
         },
-        children: [{
-          name: "출장현황",
-          path: "/admin/work/trip/list",
-          component: Mock,
-          isShow: false,
-          isOn: false
-        }]
+        children: [
+          {
+            name: "출장현황",
+            path: "/admin/work/trip/list",
+            component: Mock,
+            isShow: false,
+            isOn: false,
+            meta: {
+              rule: ["*"]
+            }
+          }
+        ]
       }
     ]
   },
@@ -382,25 +444,32 @@ const routes = [
     name: "규정/양식관리",
     icon: "mdi-book-open-variant",
     path: "/admin/law",
-    component: Blank,
+    component: AdminLayout,
     isShow: false,
     isOn: false,
     meta: {
       isDirectory: true
     },
-    children: [{
-      name: "규정",
-      path: "/admin/law/rule",
-      component: Mock,
-      isShow: false,
-      isOn: false,
-    },
+    children: [
+      {
+        name: "규정",
+        path: "/admin/law/rule",
+        component: Mock,
+        isShow: false,
+        isOn: false,
+        meta: {
+          rule: ["*"]
+        }
+      },
       {
         name: "양식",
         path: "/admin/law/form",
         component: Mock,
         isShow: false,
         isOn: false,
+        meta: {
+          rule: ["*"]
+        }
       }
     ]
   }
---
+++ src/views/Main.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-app>
+        <v-container fluid>
+            <v-sheet class="logo transparent" style="width: 10rem">
+                <span class="stress">K</span>orea
+                National
+                University
+                of <span class="stress">Arts</span>
+
+                Industry
+                <span class="stress">A</span>cademy
+                <span class="stress">Co</span>operation
+                Foundation
+            </v-sheet>
+            <div style="position: absolute; left: 0; top: 0; display: none">
+                <div class="logo-ko" style="font-size: 5rem; left: 12rem; top: 3rem;">한</div>
+                <div class="logo-ko" style="font-size: 3.5rem; left: 15rem; top: 8rem;">국</div>
+                <div class="logo-ko" style="font-size: 4rem; left: 20rem; top: 5rem;">예</div>
+                <div class="logo-ko" style="font-size: 3rem; left: 20rem; top: 9rem;">술</div>
+                <div class="logo-ko" style="font-size: 6rem; left: 15rem; top: 11rem;">종</div>
+                <div class="logo-ko" style="font-size: 4.5rem; left: 20rem; top: 15rem;">합</div>
+                <div class="logo-ko" style="font-size: 4rem; left: 18rem; top: 19rem;">학</div>
+                <div class="logo-ko" style="font-size: 4.5rem; left: 18rem; top: 22.5rem;">교</div>
+            </div>
+            <div style="position: absolute; left: 13rem; top: 5rem; display: none">
+                <div class="logo-ko" style="font-size: 5rem; left: 12rem; top: 3rem;">산</div>
+                <div class="logo-ko" style="font-size: 3.5rem; left: 15rem; top: 8rem;">학</div>
+                <div class="logo-ko" style="font-size: 4rem; left: 14rem; top: 11rem;">협</div>
+                <div class="logo-ko" style="font-size: 3rem; left: 13rem; top: 15rem;">력</div>
+                <div class="logo-ko" style="font-size: 3.5rem; left: 14rem; top: 18rem;">단</div>
+            </div>
+            <div style="position: absolute; right: 10rem; top: 10rem; width: 20rem;">
+                <v-text-field
+                        label="아이디"
+                        autofocus
+                >
+                </v-text-field>
+                <v-text-field
+                        label="암호"
+                >
+                </v-text-field>
+                <v-btn
+                        outlined
+                        class="mt-8"
+                >
+                    확인
+                </v-btn>
+            </div>
+        </v-container>
+    </v-app>
+</template>
+<style scoped>
+    .logo {
+        font-family: 'Inconsolata', monospace;
+        font-size: 2.5rem;
+    }
+    .logo-ko {
+        font-family: 'RIDIBatang';
+        position: absolute;
+    }
+    .stress {
+        color: white;
+        background: black;
+        border-radius: 0.4rem;
+        padding: 0 0.4rem 0 0.4rem;
+    }
+</style>
